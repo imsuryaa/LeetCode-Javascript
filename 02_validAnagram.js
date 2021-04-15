a301aa564a431b307e83c8b47e6f913e241e5997
suryatejaj97 <suryatejaj97@gmail.com>
2021-04-16 01:03:30 +0530

    #242 - valid anagram

--- 02_validAnagram.js
+++ 02_validAnagram.js
@@ -1,4 +1,6 @@
-// Valid Anagram
+// LeetCode - 242
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
 var isAnagram = function(s, t) {
     s = s.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('')
     t = t.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('')
